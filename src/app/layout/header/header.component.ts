565aedbe4305c5e6e289d13318bac20e4b1c3de8
malucefe <lufemarc@gmail.com>
2025-03-29 10:45:31 -0500

    Creación del header

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  constructor(private router: Router) {}
+  navigateToHome() {
+    this.router.navigate(['/home']);
+  }
+}
